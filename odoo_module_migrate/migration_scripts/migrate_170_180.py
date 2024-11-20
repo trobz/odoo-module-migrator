f549d0dc62a12b18e583e2a73d1b90f46bd92bfd
Lukas Tran <nhant@trobz.com>
2024-11-20 15:46:13 +0700

    [IMP] remove depcreate class `oe_kanban_global_click` and `oe_kanban_global_click_edit`

--- odoo_module_migrate/migration_scripts/migrate_170_180.py
+++ odoo_module_migrate/migration_scripts/migrate_170_180.py
@@ -245,10 +245,32 @@ def replace_odoo_module_from_js_assets(
             logger.error(f"Error processing file {file}: {str(e)}")
 
 
+def remove_deprecated_kanban_click_classes(
+    logger, module_path, module_name, manifest_path, migration_steps, tools
+):
+    files_to_process = tools.get_files(module_path, (".xml",))
+
+    replaces = {
+        "oe_kanban_global_click_edit": "",
+        "oe_kanban_global_click": "",
+    }
+
+    for file in files_to_process:
+        try:
+            tools._replace_in_file(
+                file,
+                replaces,
+                log_message=f"Remove deprecated kanban click classes in file: {file}",
+            )
+        except Exception as e:
+            logger.error(f"Error processing file {file}: {str(e)}")
+
+
 class MigrationScript(BaseMigrationScript):
     _GLOBAL_FUNCTIONS = [
         replace_unaccent_parameter,
         replace_deprecated_kanban_box_card_menu,
+        remove_deprecated_kanban_click_classes,
         replace_tree_with_list_in_views,
         replace_chatter_blocks,
         replace_user_has_groups,
